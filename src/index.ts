da94cc3a2dfe13696ee899ed02edd37b841bcd00
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-05-25 10:08:45 -0300

    todo list finalizado

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,62 @@
-console.log("teste do script")
+let listElement = document.querySelector("#app ul") as HTMLUListElement;
+let inputElement = document.querySelector("#app input") as HTMLInputElement;
+let btnElement = document.querySelector("#app button") as HTMLButtonElement;
+
+let listaSalva: string | null = localStorage.getItem("@listagem_tarefas");
+
+let tarefas: Array<string> =
+   (listaSalva !== null && JSON.parse(listaSalva)) || [];
+
+function listarTarefas() {
+   listElement.innerHTML = "";
+
+   tarefas.map((item) => {
+      let todoElement = document.createElement("li");
+      let tarefaText = document.createTextNode(item);
+
+      let linkElement = document.createElement("a");
+      linkElement.setAttribute("href", "#");
+
+      let posicao: number = tarefas.indexOf(item);
+
+      linkElement.setAttribute("onclick", `deletarTarefa(${posicao})`);
+      linkElement.setAttribute("style", "margin-left: 10px");
+
+      let linkText = document.createTextNode("Excluir");
+      linkElement.appendChild(linkText);
+
+      todoElement.appendChild(tarefaText);
+      todoElement.appendChild(linkElement);
+      listElement.appendChild(todoElement);
+   });
+}
+
+listarTarefas();
+
+function adicionarTarefa(): boolean | void {
+   if (inputElement.value === "") {
+      alert("Digite alguma tarefa");
+      return false;
+   } else {
+      let tarefaDigitada: string = inputElement.value;
+      tarefas.push(tarefaDigitada);
+
+      inputElement.value = "";
+      console.log(tarefas);
+      listarTarefas();
+      salvarDados();
+   }
+}
+
+function salvarDados(): void {
+   localStorage.setItem("@listagem_tarefas", JSON.stringify(tarefas));
+}
+
+function deletarTarefa(posicao: number) {
+   tarefas.splice(posicao, 1);
+
+   listarTarefas();
+   salvarDados();
+}
+
+btnElement.onclick = adicionarTarefa;
